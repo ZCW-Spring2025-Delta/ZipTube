ac4b46d79eb9deb55287bcea9346e97a873d3b4c
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-05-02 16:17:21 -0400

    repositories created

---
+++ src/main/java/ZCWDelta/ZipTube/repos/UserLibraryRepo.java
@@ -0,0 +1,7 @@
+package ZCWDelta.ZipTube.repos;
+
+import ZCWDelta.ZipTube.models.UserLibrary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserLibraryRepo extends JpaRepository<UserLibrary, Integer> {
+}
