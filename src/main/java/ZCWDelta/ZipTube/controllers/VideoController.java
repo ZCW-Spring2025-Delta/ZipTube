b24598f57e115c805aea533949a9e184ecdfddd5
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-05-02 17:35:12 -0400

    controller and service for video is done

--- src/main/java/ZCWDelta/ZipTube/controllers/VideoController.java
+++ src/main/java/ZCWDelta/ZipTube/controllers/VideoController.java
@@ -1,4 +1,53 @@
 package ZCWDelta.ZipTube.controllers;
 
+import ZCWDelta.ZipTube.models.Video;
+import ZCWDelta.ZipTube.services.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/video")
 public class VideoController {
+
+    @Autowired
+    VideoService videoService;
+
+    public VideoController(VideoService videoService){
+        this.videoService = videoService;
+    }
+    @GetMapping
+    public ResponseEntity<Iterable<Video>> getVideos(){
+        return new ResponseEntity<>(videoService.showAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Video> getVideoById(Integer Id){
+        Video video = videoService.showById(Id);
+        return new ResponseEntity<>(video, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Video> create(Video video){
+        Video newVideo = videoService.create(video);
+        return new ResponseEntity<>(video, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Video> update(Video video){
+        Video newVideo = videoService.update(video.getVideoId(), video);
+        return new ResponseEntity<>(newVideo, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(Video video){
+        videoService.delete(video);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/ZCWDelta/ZipTube/services/VideoService.java
+++ src/main/java/ZCWDelta/ZipTube/services/VideoService.java
@@ -1,4 +1,55 @@
 package ZCWDelta.ZipTube.services;
 
+import ZCWDelta.ZipTube.models.Video;
+import ZCWDelta.ZipTube.repos.VideoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Component
 public class VideoService {
+
+    @Autowired
+    VideoRepo videoRepo;
+
+    public Iterable<Video> showAll() {
+        return videoRepo.findAll();
+    }
+    public Video getVideoById(Integer videoId) {
+        return videoRepo.getReferenceById(videoId);
+    }
+
+    public Video showById(Integer videoId) {
+        return videoRepo.findById(videoId).get();
+    }
+
+    public Video create(Video video) {
+        return videoRepo.save(video);
+    }
+
+    public Video update(Integer videoId, Video video) {
+        Video originalVideo = videoRepo.findById(videoId).get();
+        originalVideo.setVideoName(video.getVideoName());
+        originalVideo.setQuery(video.getQuery());
+        originalVideo.setURL(video.getURL());
+        originalVideo.setYear(video.getYear());
+        originalVideo.setFavorite(video.getFavorite());
+        return videoRepo.save(originalVideo);
+    }
+
+    public Boolean delete(Video video) {
+        Optional<Video> itemOptional = videoRepo.findById(video.getVideoId());
+
+        if (itemOptional.isPresent()) {
+            videoRepo.deleteById(video.getVideoId());
+            return true; // successfully deleted
+        } else {
+            return false; // no item found to delete
+        }
+    }
+
+
 }
