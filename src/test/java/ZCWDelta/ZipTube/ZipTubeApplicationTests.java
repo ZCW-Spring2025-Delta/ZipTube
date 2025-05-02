a5a61baa79c177c84adc8c2cc11f776d63bb951b
Nathan <natefintak@gmail.com>
2025-05-02 15:41:42 -0400

    initial commit

---
+++ src/test/java/ZCWDelta/ZipTube/ZipTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package ZCWDelta.ZipTube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
