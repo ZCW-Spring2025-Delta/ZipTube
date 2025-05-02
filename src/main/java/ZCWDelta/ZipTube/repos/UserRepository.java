edb3d1fa65ac3178dfd1689f288412ef3fd1884a
GabrielC67 <gabecruzpro@gmail.com>
2025-05-02 17:29:01 -0400

    final commit of the day

---
+++ src/main/java/ZCWDelta/ZipTube/repos/UserRepository.java
@@ -0,0 +1,8 @@
+package ZCWDelta.ZipTube.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <String, Integer> {
+}
