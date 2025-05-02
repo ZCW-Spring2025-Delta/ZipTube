7f78b6daab73be9b71330bdc67826dec4d76b3d7
Nathan <natefintak@gmail.com>
2025-05-02 17:28:35 -0400

    Comment classes in progress

--- src/main/java/ZCWDelta/ZipTube/services/CommentService.java
+++ src/main/java/ZCWDelta/ZipTube/services/CommentService.java
@@ -1,4 +1,42 @@
 package ZCWDelta.ZipTube.services;
 
+
+import ZCWDelta.ZipTube.models.Comment;
+import ZCWDelta.ZipTube.repos.CommentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@Component
 public class CommentService {
+
+    @Autowired
+    private CommentRepo repo;
+
+    public CommentService(CommentRepo repo) {
+        this.repo = repo;
+    }
+
+    public Iterable<Comment> getAllComments() {
+        return repo.findAll();
+    }
+
+    public Comment show(Integer id) {
+        return repo.findById(id).get();
+    }
+
+    public Iterable<Comment> findByVideoId(Integer videoId) {
+        ArrayList<Comment> comments = new ArrayList<>();
+        for (Comment comment : repo.findAll()) {
+            if (comment.getVideoId() == videoId) {
+                comments.add(comment);
+            }
+        }
+        return comments;
+    }
+
+
 }
