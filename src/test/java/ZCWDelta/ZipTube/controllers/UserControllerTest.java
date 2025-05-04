1cc4d4d777cc921ebebb14c9b6932debf2588a84
GabrielC67 <gabecruzpro@gmail.com>
2025-05-04 15:46:33 -0400

    Mid day commit

--- src/test/java/ZCWDelta/ZipTube/controllers/UserControllerTest.java
+++ src/test/java/ZCWDelta/ZipTube/controllers/UserControllerTest.java
@@ -1,4 +1,25 @@
 package ZCWDelta.ZipTube.controllers;
 
+import org.junit.jupiter.api.Test;
+
 public class UserControllerTest {
+    @Test
+    void testCreateUser() {
+    }
+
+    @Test
+    void testGetAllUsers() {
+    }
+
+    @Test
+    void testGetUserById() {
+    }
+
+    @Test
+    void testUpdateUser() {
+    }
+
+    @Test
+    void testDeleteUser() {
+    }
 }
