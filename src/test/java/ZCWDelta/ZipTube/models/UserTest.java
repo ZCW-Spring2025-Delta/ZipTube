b39719778ca9859b0a6787c51b8b3bda8566c32d
GabrielC67 <gabecruzpro@gmail.com>
2025-05-04 10:49:54 -0400

    Change needed for linking tables as per request from teammate. UserTest in progress.

--- src/test/java/ZCWDelta/ZipTube/models/UserTest.java
+++ src/test/java/ZCWDelta/ZipTube/models/UserTest.java
@@ -1,4 +1,85 @@
 package ZCWDelta.ZipTube.models;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserTest {
+    User user = new User();
+
+
+    @Test
+    void defaultConstructorTest(){
+    }
+
+    @Test
+    void constructorTest(){
+
+    }
+
+    @Test
+    void getUsername() {
+        //Given
+        String username = "CodeNinja67";
+
+        //When
+        user.setUsername(username);
+
+        String expected = username;
+
+        String actual = user.getUsername();
+
+        //Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getFirstName() {
+        //Given
+        String firstName = "John";
+
+        user.setFirstName(firstName);
+
+        String expected = firstName;
+
+        String actual = user.getFirstName();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getLastName() {
+        String lastName = "Doe";
+
+        user.setLastName(lastName);
+
+        String actual = user.getLastName();
+
+        assertEquals(lastName, actual);
+
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getFavorites() {
+    }
+
+    @Test
+    void getUpload() {
+    }
+
+    @Test
+    void getCommentId() {
+    }
+
+    @Test
+    void getUserLibraryId() {
+    }
 }
