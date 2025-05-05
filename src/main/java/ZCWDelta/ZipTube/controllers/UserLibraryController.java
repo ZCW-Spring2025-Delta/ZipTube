7745329351e7de4319ff70797f6ed3d759de3011
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-05-05 13:52:57 -0400

    checkpoint

--- src/main/java/ZCWDelta/ZipTube/controllers/UserLibraryController.java
+++ src/main/java/ZCWDelta/ZipTube/controllers/UserLibraryController.java
@@ -1,6 +1,7 @@
 package ZCWDelta.ZipTube.controllers;
 
 import ZCWDelta.ZipTube.models.User;
+import ZCWDelta.ZipTube.models.UserLibrary;
 import ZCWDelta.ZipTube.models.Video;
 import ZCWDelta.ZipTube.services.UserLibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/library")
+@RequestMapping("/user/library")
 public class UserLibraryController {
 
     @Autowired
@@ -18,15 +21,44 @@ public class UserLibraryController {
     public UserLibraryController(UserLibraryService userLibraryService){
         this.userLibraryService = userLibraryService;
     }
+    @GetMapping
+    public ResponseEntity<List<UserLibrary>> show(UserLibrary userLibrary){
+        return new ResponseEntity<>(userLibraryService.showAll(), HttpStatus.CREATED);
+    }
 
     @GetMapping("/favorites")
     public ResponseEntity<Iterable<Video>> getAllFavorites(@PathVariable Integer id, @RequestBody User user){
         return new ResponseEntity<>(userLibraryService.showAllFavorites(id, user), HttpStatus.OK);
     }
+    @GetMapping("/favorites/{id}")
+    public ResponseEntity<Video> getFavoriteVideoById(@PathVariable Integer videoId, @RequestBody User user){
+        List<Video> favorites = userLibraryService.showAllFavorites(user.getId(), user);
+        return new ResponseEntity<>(favorites.get(videoId), HttpStatus.OK);
+    }
 
     @GetMapping("/uploads")
     public ResponseEntity<Iterable<Video>> getAllUploads(@PathVariable Integer id, @RequestBody User user){
         return new ResponseEntity<>(userLibraryService.showAllUploads(id, user), HttpStatus.OK);
+    }
+
+    @GetMapping("/uploads/{id}")
+    public ResponseEntity<Video> getUploadVideoById(@PathVariable Integer videoId, @RequestBody User user){
+        List<Video> uploads = userLibraryService.showAllUploads(user.getId(), user);
+        return new ResponseEntity<>(uploads.get(videoId), HttpStatus.OK);
+    }
+
+    @PostMapping("/uploads")
+    public ResponseEntity<Video> addToUploads(@PathVariable Video video, @RequestBody User user){
+        List<Video> uploads = userLibraryService.showAllUploads(user.getId(), user);
+        uploads.add(video);
+        return new ResponseEntity<>(video, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/favorites")
+    public ResponseEntity<Video> addToFavorites(@PathVariable Video video, @RequestBody User user){
+        List<Video> favorites = userLibraryService.showAllFavorites(user.getId(), user);
+        favorites.add(video);
+        return new ResponseEntity<>(video, HttpStatus.CREATED);
     }
 
 }
