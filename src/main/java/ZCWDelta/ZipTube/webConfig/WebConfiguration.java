610333bfa3e91f398b35f355ea5de6760c31452f
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-05-06 20:18:02 -0400

    added webconfib to work with CORS access

--- src/main/java/ZCWDelta/ZipTube/webConfig/WebConfiguration.java
+++ src/main/java/ZCWDelta/ZipTube/webConfig/WebConfiguration.java
@@ -1,4 +1,17 @@
 package ZCWDelta.ZipTube.webConfig;
 
-public class WebConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500") // adjust to your frontend port
+                .allowedOrigins("http://127.0.0.1:5501") // adjust to your frontend port
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
