9fb01e5d6fe78de7ab97e076256c66fc0f458bac
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-05-08 00:26:48 -0400

    deep dive into whatttt

--- src/main/java/ZCWDelta/ZipTube/repos/UserRepo.java
+++ src/main/java/ZCWDelta/ZipTube/repos/UserRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
-
+    Optional<User> findByUsernameAndPassword(String username, String password);
     Optional<User> findByUsername(String username);  // 🔐 used in authentication
     boolean existsByUsername(String username);       // for registration
 
